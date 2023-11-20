ec6be2188dfa9bff352e3e2e53f59779ff9ad6df
DELL <DELL@DESKTOP-EN72J61>
2023-11-20 14:34:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QueueMain queuemain = new QueueMain();
+            
+            Queue queue = queuemain.Enqueue();
+            queuemain.Display(queue);
+            queuemain.Dequeue(queue);
+            queuemain.Peek(queue);
+            if (queuemain.Contains(queue))
+            {
+                Console.WriteLine("\nElement Present");
+            }
+            else
+            {
+                Console.WriteLine("\nElement Not Present");
+            }
+            queuemain.Count(queue);
+            queuemain.Clear(queue);
+            queuemain.Display(queue);
+        }
+    }
+}
---
+++ QueueMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    public class QueueMain
+    {
+        Queue queue = null;
+        public QueueMain() 
+        {
+            if(queue == null)
+            {
+                queue = new Queue();
+            }
+        }
+        public bool Check(Queue queue)
+        {
+            if (queue != null && queue.Count != 0)
+                return true;
+            return false;
+        }
+        public Queue Enqueue()
+        {
+            queue.Enqueue(100);
+            queue.Enqueue("Tharmesh");
+            queue.Enqueue('A');
+            queue.Enqueue(0987654321);
+            queue.Enqueue("Anna Nagar");
+            Console.WriteLine("Element Enqueued Successfully !\n");
+            return queue;
+        }
+        public void Dequeue(Queue queue)
+        {
+            queue.Dequeue();
+            Console.WriteLine("Element Dequeued Successfully !\n");
+            Display(queue);
+        }
+
+        public void Peek(Queue queue)
+        {
+            if(Check(queue))
+            {
+                Console.WriteLine("\nPeek Element in the Queue : "+queue.Peek());
+            }
+            else
+            {
+                Console.WriteLine("Queue is Empty !\n");
+            }
+        }
+        public bool Contains(Queue queue)
+        {
+            if (Check(queue))
+            {
+                return queue.Contains(1);
+            }
+            return false;
+        }
+
+        public void Clear(Queue queue)
+        {
+            if (Check(queue))
+            {
+                queue.Clear();
+                Console.WriteLine("\nQueue Cleared Successfully !\n");
+            }
+            else
+            {
+                Console.WriteLine("Queue Is Empty !\n");
+            }
+        }
+
+        public void Count(Queue queue)
+        {
+            if (Check(queue))
+            {
+                Console.WriteLine("\nTotal Element in the Queue "+queue.Count);
+            }
+            else
+            {
+                Console.WriteLine("Queue is Empty !\n");
+            }
+        }
+        public void Display(Queue queue)
+        {
+            int x = 0;
+            if(Check(queue))
+            {
+                foreach (var item in queue)
+                {
+                    Console.WriteLine("Element {0} in Queue is {1}", ++x, item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Queue is Empty !\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
